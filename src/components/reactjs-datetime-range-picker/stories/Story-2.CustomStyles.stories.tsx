d1cb0ff5847259619820eaa9ffb2594df12933ae
Bhavin Patel <bhavinpatel@Bhavins-MacBook-Pro.local>
2025-07-18 20:50:06 -0400

    upgrade react to 19

--- src/components/reactjs-datetime-range-picker/stories/Story-2.CustomStyles.stories.tsx
+++ src/components/reactjs-datetime-range-picker/stories/Story-2.CustomStyles.stories.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react-vite";
 import moment from "moment";
 import { ReactJSDatetimeRangePicker, DateRangeModel, Options } from "../../..";
 import { DatetimeRangeType } from "../enum";
